103aaeed4e6d6194ef9360ed70a947fec8b17b28
AndingXd <ADmitriev2005@yandex.ru>
2024-05-28 20:34:08 +0300

    lab done

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,41 +3,37 @@
 #include "main.cpp"
 #include <string>
 
-// Тест для конструктора класса ConcreteMemento
-TEST(ConcreteMemento, Constructor) {
-    const std::string expectedState = "initial state";
+TEST(MementoTest, ConstructorSetsState) {
+    std::string expectedState = "Hello";
     ConcreteMemento memento(expectedState);
 
-    EXPECT_EQ(memento.state(), expectedState);
+    ASSERT_EQ(memento.state(), expectedState);
 }
 
-// Тест для метода state() класса ConcreteMemento
-TEST(ConcreteMemento, State) {
-    const std::string state = "current state";
-    ConcreteMemento memento(state);
+TEST(TextEditorTest, AppendText) {
+    TextEditor editor;
 
-    EXPECT_EQ(memento.state(), state);
+    editor.AppendText("Hello");
+    ASSERT_EQ(editor._currentText, "Hello");
+
+    editor.AppendText(", world!");
+    ASSERT_EQ(editor._currentText, "Hello, world!");
 }
 
-// Тест для метода Undo() класса Logger
-TEST(Logger, Undo) {
+TEST(LoggerTest, UndoRestoresPreviousState) {
     TextEditor editor;
     Logger logger(&editor);
 
-    // Добавляем текст к редактору
-    editor.AppendText("initial text");
-
-    // Создаем резервную копию состояния
+    editor.AppendText("Hello");
     logger.Backup();
 
-    // Добавляем больше текста к редактору
-    editor.AppendText(" additional text");
+    editor.AppendText(" Dear Amin");
+    editor.Print();
+    std::string expectedOutput = "Hello Dear Amin";
 
-    // Восстанавливаем состояние из резервной копии
     logger.Undo();
-
-    // Проверяем, что изменения, внесенные после создания резервной копии, были отменены
-    EXPECT_EQ(editor.GetText(), "initial text");
+    editor.Print();
+    ASSERT_EQ(testing::internal::GetCapturedStdout(), expectedOutput);
 }
 
 
